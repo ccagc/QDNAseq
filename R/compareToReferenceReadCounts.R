424925110b4b45d0fcf72d5fbb206c6a9d08e34f
Ilari Scheinin <ilari.scheinin@gmail.com>
2013-06-04 17:31:37 +0200

    Added a function for comparative analysis using reference samples.

---
+++ R/compareToReferenceReadCounts.R
@@ -0,0 +1,45 @@
+#########################################################################/**
+# @RdocFunction compareToReferenceReadCounts
+#
+# @title "Divide binned read counts with those of reference sample(s)"
+#
+# @synopsis
+#
+# \description{
+#  @get "title".
+# }
+#
+# \arguments{
+#   \item{obj}{...}
+#   \item{reference}{...}
+# }
+#
+# \value{
+#   Returns a named @list containing elements ...
+# }
+#
+# @author "IS"
+#
+# \seealso{
+#   Internally, ...
+# }
+#
+#*/#########################################################################
+compareToReferenceReadCounts <- function(obj, references) {
+  if (length(references) != ncol(obj))
+    stop('Vector references must be of equal length as there are samples in obj.')
+  for (i in seq_along(references)) {
+    if (!is.na(references[i]) && references[i]!=FALSE) {
+      assayDataElement(obj, 'counts')[,i] <- (assayDataElement(obj, 'counts')[,i]+1) / (assayDataElement(obj, 'counts')[,references[i]]+1)
+      if ('corrected' %in% assayDataElementNames(obj))
+        assayDataElement(obj, 'corrected')[,i] <- (assayDataElement(obj, 'corrected')[,i]+1) / (assayDataElement(obj, 'corrected')[,references[i]]+1)
+      sampleNames(obj)[i] <- paste(sampleNames(obj)[i], ' vs. ', sampleNames(obj)[references[i]], sep='')
+    }
+  }
+  toremove <- which(references==FALSE)
+  if (length(toremove)>0)
+    obj <- obj[,-toremove]
+  obj
+}
+
+# EOF
