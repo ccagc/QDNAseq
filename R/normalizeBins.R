2bfb842c247d75082c07f35535af242e8096c148
Ilari Scheinin <ilari.scheinin@gmail.com>
2014-01-21 17:04:55 +0100

    Update NormalizeBins() to normalize with division when not doing a log2-transformation (instead of the addition which is used with transformed data). Also add an option to normalize using the mean, some documentation, and pass … arguments to DNAcopy::smooth.CNA.

--- R/normalizeBins.R
+++ R/normalizeBins.R
@@ -12,54 +12,56 @@
 # }
 #
 # \arguments{
-#   \item{object}{...}
-#   \item{method}{A @character string specifying ...}
-#   \item{smoothOutliers}{If @TRUE, ..., otherwise, ...}
-#   \item{logTransform}{If @TRUE, ..., otherwise, ...}
-#   \item{logOffset}{...}
-#   \item{force}{...}
-#   \item{...}{Not used.}
+#   \item{object}{A QDNAseqReadCounts object.}
+#   \item{method}{A @character string specifying the normalization method.
+#     Choices are "none", "mean", "median" (default), or "mode". A partial
+#     string sufficient to uniquely identify the choice is permitted.}
+#   \item{smoothOutliers}{If @TRUE (default), smooth.CNA from package DNAcopy is
+#     used detect and smooth outliers.}
+#   \item{logTransform}{If @TRUE (default), data will be log2-transformed.}
+#   \item{logOffset}{An offset to be added prior to log2-transformation to
+#     avoid non-positive numbers. Ignored if logTransform=@FALSE.}
+#   \item{force}{Running this function will remove possible segmentation and
+#     calling results. When they are present, running requires specifying
+#     force=@TRUE.}
+#   \item{...}{Further arguments to DNAcopy::smooth.CNA.}
 # }
 #
 # \value{
-#   Returns a named @list containing elements ...
+#   Returns a QDNAseqReadCounts object with normalized data.
 # }
 #
 # @author "IS"
 #
-# \seealso{
-#   Internally, ...
-# }
-#
 #*/#########################################################################
 ## Adapted from CGHcall::normalize()
-setMethod('normalizeBins', signature=c(object='QDNAseqReadCounts'),
-  definition=function(object, method='median', smoothOutliers=TRUE,
+setMethod("normalizeBins", signature=c(object="QDNAseqReadCounts"),
+  definition=function(object, method="median", smoothOutliers=TRUE,
   logTransform=TRUE, logOffset=2^-10, force=FALSE, ...) {
 
-  if (!force && 'segmented' %in% assayDataElementNames(object))
-    stop('Data has already been segmented. Changing the normalization will ',
-      'remove segmentation (and possible calling) results. Please specify ',
-      'force=TRUE, if you want this.')
-  if ('segmented' %in% assayDataElementNames(object))
-    assayDataElement(object, 'segmented') <- NULL
-  if ('calls' %in% assayDataElementNames(object)) {
-    assayDataElement(object, 'calls') <- NULL
-    assayDataElement(object, 'probloss') <- NULL
-    assayDataElement(object, 'probnorm') <- NULL
-    assayDataElement(object, 'probgain') <- NULL
-    if ('probdloss' %in% assayDataElementNames(object))
-      assayDataElement(object, 'probdloss') <- NULL
-    if ('probamp' %in% assayDataElementNames(object))
-      assayDataElement(object, 'probamp') <- NULL
+  if (!force && "segmented" %in% assayDataElementNames(object))
+    stop("Data has already been segmented. Changing the normalization will ",
+      "remove segmentation (and possible calling) results. Please specify ",
+      "force=TRUE, if you want this.")
+  if ("segmented" %in% assayDataElementNames(object))
+    assayDataElement(object, "segmented") <- NULL
+  if ("calls" %in% assayDataElementNames(object)) {
+    assayDataElement(object, "calls") <- NULL
+    assayDataElement(object, "probloss") <- NULL
+    assayDataElement(object, "probnorm") <- NULL
+    assayDataElement(object, "probgain") <- NULL
+    if ("probdloss" %in% assayDataElementNames(object))
+      assayDataElement(object, "probdloss") <- NULL
+    if ("probamp" %in% assayDataElementNames(object))
+      assayDataElement(object, "probamp") <- NULL
   }
 
   # Extract corrected counts
-  copynumber <- assayDataElement(object, 'corrected')
+  copynumber <- assayDataElement(object, "corrected")
 
   # Sanity check
   if (is.null(copynumber)) {
-    stop('Cannot normalize bins. Please run correctBins() first.')
+    stop("Cannot normalize bins. Please run correctBins() first.")
   }
 
   # Extract annotation data
@@ -79,39 +81,51 @@ setMethod('normalizeBins', signature=c(object='QDNAseqReadCounts'),
   # Filter?
   condition <- binsToUse(object)
 
-  if (method == 'none') {
-    vmsg('Skipping normalization ...')
+  if (match.arg(method, c("mean", "median", "mode", "none")) ==
+    "none") {
+    vmsg("Skipping normalization ...")
   } else {
-    if (method == 'median') {
-      vmsg('Applying median normalization ...')
+    if (match.arg(method, c("mean", "median", "mode", "none")) ==
+      "mean") {
+      vmsg("Applying mean normalization ...")
+      values <- colMeans(copynumber[condition, , drop=FALSE], na.rm=TRUE)
+    } else if (match.arg(method, c("mean", "median", "mode", "none")) ==
+      "median") {
+      vmsg("Applying median normalization ...")
       values <- colMedians(copynumber[condition, , drop=FALSE], na.rm=TRUE)
-    } else if (method == 'mode') {
-      vmsg('Applying mode normalization ... ')
+    } else if (match.arg(method, c("mean", "median", "mode", "none")) ==
+      "mode") {
+      vmsg("Applying mode normalization ... ")
       values <- apply(copynumber[condition, , drop=FALSE], MARGIN=2L,
         FUN=function(x) {
         d <- density(x, na.rm=TRUE); d$x[which.max(d$y)]
       })
     }
-    copynumber <- t(t(copynumber) - values)
+    # Normalize by subtraction (log) or division (linear):
+    if (logTransform) {
+      copynumber <- scale(copynumber, center=values, scale=FALSE)
+    } else {
+      copynumber <- scale(copynumber, center=FALSE, scale=values)
+    }
   }
 
   # Smooth outliers?
   if (smoothOutliers) {
-    vmsg('Smoothing outliers ...')
-    CNA.object <- CNA(copynumber, chrom=fData[,'chromosome'],
-      maploc=fData[,'start'], data.type='logratio', presorted=TRUE)
-    CNA.object <- smooth.CNA(CNA.object)
+    vmsg("Smoothing outliers ...")
+    CNA.object <- CNA(copynumber, chrom=fData[,"chromosome"],
+      maploc=fData[,"start"], data.type="logratio", presorted=TRUE)
+    CNA.object <- smooth.CNA(CNA.object, ...)
     CNA.object <- CNA.object[, -(1:2), drop=FALSE]
     copynumber <- as.matrix(CNA.object)
   }
 
   # Expand to full set of bins
   copynumber2 <- matrix(NA_real_, nrow=nrow(object), ncol=ncol(object),
-               dimnames=list(featureNames(object), sampleNames(object)))
+    dimnames=list(featureNames(object), sampleNames(object)))
   copynumber2[rownames(copynumber), ] <- copynumber
 
   # Assign
-  assayDataElement(object, 'copynumber') <- copynumber2
+  assayDataElement(object, "copynumber") <- copynumber2
 
   object
 })
