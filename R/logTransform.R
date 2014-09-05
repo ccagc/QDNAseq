f109d7cff409f747c4919659a8bc92afd29d5027
Daoud Sie <d.sie@vumc.nl>
2014-09-05 11:59:20 -0400

    Modified segment function to take in a function for data transformation. Currently log2 (default) and sqrt transformation are implemented.

--- R/logTransform.R
+++ R/logTransform.R
@@ -19,16 +19,30 @@ log2offset <- function(offset=.Machine$double.xmin) {
     offset;
 }
 
-log2adhoc <- function(x, offset=log2offset()) {
-    x[x < 0] <- 0
-    x <- x + offset
-    log2(x)
-
+log2adhoc <- function(x, offset=log2offset(), inv=FALSE) {
+    if (!inv) {
+      x[x < 0] <- 0
+      x <- x + offset
+      log2(x)
+    } else {
+      x <- 2^x
+      x - offset
+    }
 }
 
 unlog2adhoc <- function(y, offset=log2offset()) {
     y <- 2^y
     y - offset
+}
+
+sqrtadhoc <- function(x, factor=3/8, offset=0, inv=FALSE) {
+  if (!inv) {
+    x <- x + offset
+    sqrt(x * factor)
+  } else {
+    x <- x^2 * (factor^-1)
+    x - offset
+  }  
 }
 
 # EOF
