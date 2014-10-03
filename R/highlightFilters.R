fc6030d2510c1b56992a949bdbe1ea2fd50194be
Daoud Sie <d.sie@vumc.nl>
2014-10-03 17:42:13 -0400

    Complied to bioconductor code style on indentation

--- R/highlightFilters.R
+++ R/highlightFilters.R
@@ -49,130 +49,133 @@
 
 setMethod("highlightFilters", signature=c(object="QDNAseqSignals"),
     definition=function(object, col="red", residual=NA, blacklist=NA,
-    mappability=NA, bases=NA, type=c("union", "intersection"), ...) {
-
-    condition <- rep(TRUE, times=nrow(object))
-    type <- match.arg(type)
-    if (type == "intersection") {
-        if (!is.na(residual)) {
-            if (is.numeric(residual)) {
-                condition <- condition & (is.na(fData(object)$residual) |
-                    abs(fData(object)$residual) > residual)
-            } else if (residual) {
-                condition <- condition & is.na(fData(object)$residual)
+        mappability=NA, bases=NA, type=c("union", "intersection"), ...) {
+        
+        condition <- rep(TRUE, times=nrow(object))
+        type <- match.arg(type)
+        if (type == "intersection") {
+            if (!is.na(residual)) {
+                if (is.numeric(residual)) {
+                    condition <- condition & (is.na(fData(object)$residual) |
+                            abs(fData(object)$residual) > residual)
+                } else if (residual) {
+                    condition <- condition & is.na(fData(object)$residual)
+                }
             }
-        }
-        if (!is.na(blacklist)) {
-            if (is.numeric(blacklist)) {
-                condition <- condition & fData(object)$blacklist > blacklist
-            } else if (blacklist) {
-                condition <- condition & fData(object)$blacklist != 0
+            if (!is.na(blacklist)) {
+                if (is.numeric(blacklist)) {
+                    condition <- condition & fData(object)$blacklist > blacklist
+                } else if (blacklist) {
+                    condition <- condition & fData(object)$blacklist != 0
+                }
             }
-        }
-        if (!is.na(mappability) && mappability != FALSE)
-            condition <- condition & fData(object)$mappability < mappability
-        if (!is.na(bases) && bases != FALSE)
-            condition <- condition & fData(object)$bases < bases
-    } else {
-        if (!is.na(residual)) {
-            if (is.numeric(residual)) {
-                condition <- condition & !is.na(fData(object)$residual) &
-                    abs(fData(object)$residual) <= residual
-            } else if (residual) {
-                condition <- condition & !is.na(fData(object)$residual)
+            if (!is.na(mappability) && mappability != FALSE)
+                condition <- condition & fData(object)$mappability < mappability
+            if (!is.na(bases) && bases != FALSE)
+                condition <- condition & fData(object)$bases < bases
+        } else {
+            if (!is.na(residual)) {
+                if (is.numeric(residual)) {
+                    condition <- condition & !is.na(fData(object)$residual) &
+                        abs(fData(object)$residual) <= residual
+                } else if (residual) {
+                    condition <- condition & !is.na(fData(object)$residual)
+                }
             }
-        }
-        if (!is.na(blacklist)) {
-            if (is.numeric(blacklist)) {
-                condition <- condition & fData(object)$blacklist <= blacklist
-            } else if (blacklist) {
-                condition <- condition & fData(object)$blacklist == 0
+            if (!is.na(blacklist)) {
+                if (is.numeric(blacklist)) {
+                    condition <- condition & 
+                        (fData(object)$blacklist <= blacklist)
+                } else if (blacklist) {
+                    condition <- condition & fData(object)$blacklist == 0
+                }
             }
+            if (!is.na(mappability) && mappability != FALSE)
+                condition <- condition & 
+                (fData(object)$mappability >= mappability)
+            if (!is.na(bases) && bases != FALSE)
+                condition <- condition & fData(object)$bases >= bases
+            condition <- !condition
         }
-        if (!is.na(mappability) && mappability != FALSE)
-            condition <- condition & fData(object)$mappability >= mappability
-        if (!is.na(bases) && bases != FALSE)
-            condition <- condition & fData(object)$bases >= bases
-        condition <- !condition
-    }
-
-    i <- 1
-    chrs <- unique(fData(object)$chromosome[binsToUse(object)])
-    condition <- condition & fData(object)$bases > 0 &
-        fData(object)$chromosome %in% chrs
-
-    all.chrom <- chromosomes(object)
-    chrom <- all.chrom[condition]
-    uni.chrom <- unique(chrom)
-    if (!getOption("QDNAseq::plotScale")) {
-        index <- 1:nrow(object)
-        indexPos <- rep(NA_integer_, times=nrow(object))
-        indexPos[binsToUse(object)] <- 1:sum(binsToUse(object))
-        f <- approxfun(index, indexPos)
-        pos <- f(index[condition])
-    } else {
-        all.chrom.lengths <- aggregate(bpend(object),
-            by=list(chromosome=all.chrom), FUN=max)
-        chrom.lengths <- all.chrom.lengths$x
-        names(chrom.lengths) <- all.chrom.lengths$chromosome
-
-        pos <- as.numeric(bpstart(object)[condition])
-        chrom.lengths <- chrom.lengths[as.character(uni.chrom)]
-        for (j in uni.chrom)
-            pos[chrom > j] <- pos[chrom > j] + chrom.lengths[as.character(j)]
-    }
-    if (class(object) == "QDNAseqReadCounts") {
-        copynumber <- assayDataElement(object, "counts")[condition, ,
-            drop=FALSE]
-    } else {
-        copynumber <- assayDataElement(object, "copynumber")[condition, ,
-            drop=FALSE]
-    }
-    if (getOption("QDNAseq::plotLogTransform"))
-        copynumber <- log2adhoc(copynumber)
-    if (ncol(object) > 1L)
-        vmsg("Multiple samples present in input, only using first sample: ",
-            sampleNames(object)[1L])
-    pointcol <- col
-    ylim <- par("usr")[3:4]
-    points(pos, copynumber[, i], cex=0.1, col=pointcol)
-    amps <- copynumber[, i]
-    amps[amps <= ylim[2]] <- NA_real_
-    amps[!is.na(amps)] <- ylim[2] + 0.01 * (ylim[2]-ylim[1])
-    dels <- copynumber[, i]
-    dels[dels >= ylim[1]] <- NA_real_
-    dels[!is.na(dels)] <- ylim[1] - 0.01 * (ylim[2]-ylim[1])
-    par(xpd=TRUE)
-    points(pos, amps, pch=24, col=pointcol, bg=pointcol, cex=0.5)
-    points(pos, dels, pch=25, col=pointcol, bg=pointcol, cex=0.5)
-    par(xpd=FALSE)
-
-    num <- sum(condition)
-    vmsg("Highlighted ", format(num, big.mark=","), " bins.")
-    return(invisible(num))
-})
+        
+        i <- 1
+        chrs <- unique(fData(object)$chromosome[binsToUse(object)])
+        condition <- condition & fData(object)$bases > 0 &
+            fData(object)$chromosome %in% chrs
+        
+        all.chrom <- chromosomes(object)
+        chrom <- all.chrom[condition]
+        uni.chrom <- unique(chrom)
+        if (!getOption("QDNAseq::plotScale")) {
+            index <- 1:nrow(object)
+            indexPos <- rep(NA_integer_, times=nrow(object))
+            indexPos[binsToUse(object)] <- 1:sum(binsToUse(object))
+            f <- approxfun(index, indexPos)
+            pos <- f(index[condition])
+        } else {
+            all.chrom.lengths <- aggregate(bpend(object),
+                by=list(chromosome=all.chrom), FUN=max)
+            chrom.lengths <- all.chrom.lengths$x
+            names(chrom.lengths) <- all.chrom.lengths$chromosome
+            
+            pos <- as.numeric(bpstart(object)[condition])
+            chrom.lengths <- chrom.lengths[as.character(uni.chrom)]
+            for (j in uni.chrom)
+                pos[chrom > j] <- 
+                    pos[chrom > j] + chrom.lengths[as.character(j)]
+        }
+        if (class(object) == "QDNAseqReadCounts") {
+            copynumber <- assayDataElement(object, "counts")[condition, ,
+                drop=FALSE]
+        } else {
+            copynumber <- assayDataElement(object, "copynumber")[condition, ,
+                drop=FALSE]
+        }
+        if (getOption("QDNAseq::plotLogTransform"))
+            copynumber <- log2adhoc(copynumber)
+        if (ncol(object) > 1L)
+            vmsg("Multiple samples present in input, only using first sample: ",
+                sampleNames(object)[1L])
+        pointcol <- col
+        ylim <- par("usr")[3:4]
+        points(pos, copynumber[, i], cex=0.1, col=pointcol)
+        amps <- copynumber[, i]
+        amps[amps <= ylim[2]] <- NA_real_
+        amps[!is.na(amps)] <- ylim[2] + 0.01 * (ylim[2]-ylim[1])
+        dels <- copynumber[, i]
+        dels[dels >= ylim[1]] <- NA_real_
+        dels[!is.na(dels)] <- ylim[1] - 0.01 * (ylim[2]-ylim[1])
+        par(xpd=TRUE)
+        points(pos, amps, pch=24, col=pointcol, bg=pointcol, cex=0.5)
+        points(pos, dels, pch=25, col=pointcol, bg=pointcol, cex=0.5)
+        par(xpd=FALSE)
+        
+        num <- sum(condition)
+        vmsg("Highlighted ", format(num, big.mark=","), " bins.")
+        return(invisible(num))
+    })
 
 # Generic highlight function
 
 highlight <- function(obj, filter, element="copynumber", ...) {
-  
-  if ( ncol(obj) != 1 ) {
-    vmsg("More than one sample given, please specifiy 1 sample only,... exiting")
-    return()
-  }
-  chroms <- as.factor(chromosomes(obj)) 
-  chrom.unique <- unique(chroms)
-  
-  chrom.lengths <- CGHbase:::.getChromosomeLengths("GRCh37")
-  
-  chrCum <- c(0, cumsum(chrom.lengths[ as.integer(as.vector(chrom.unique))]))  
-  
-  fd <- fData(obj)
-  
-  conlin <- fd$start + chrCum[as.integer(chroms)]
-  
-  cn <- log2(assayDataElement(obj, element))
-  points(conlin[filter], cn[filter], ...) 
+    
+    if ( ncol(obj) != 1 ) {
+    vmsg("More than one sample given, please specifiy 1 sample only. Exiting")
+        return()
+    }
+    chroms <- as.factor(chromosomes(obj)) 
+    chrom.unique <- unique(chroms)
+    
+    chrom.lengths <- CGHbase:::.getChromosomeLengths("GRCh37")
+    
+    chrCum <- c(0, cumsum(chrom.lengths[ as.integer(as.vector(chrom.unique))]))
+    
+    fd <- fData(obj)
+    
+    conlin <- fd$start + chrCum[as.integer(chroms)]
+    
+    cn <- log2(assayDataElement(obj, element))
+    points(conlin[filter], cn[filter], ...) 
 }
 
 
