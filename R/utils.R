456509b574268ea9cff8264cfd74eb6378b3dc24
Ilari Scheinin <ilari.scheinin@gmail.com>
2016-02-06 10:01:15 +0200

    Make package future optional

--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,24 @@
 ## Until the future package provides a well-defined *apply() API
 ## we define futurized versions of lapply() and apply() here.
-flapply <- function(x, FUN, ...) {
-  res <- list()
-  for (ii in seq_along(x)) res[[ii]] <- future(FUN(x[[ii]], ...))
-  names(res) <- names(x)
-  values(res)
+flapply <- function(X, FUN, ...) {
+    if (length(X) == 1 || !"future" %in% loadedNamespaces())
+        return(lapply(X, FUN, ...))
+    res <- list()
+    for (ii in seq_along(X)) {
+        X_ii <- X[[ii]]
+        res[[ii]] <- future::future(FUN(X_ii, ...))
+    }
+    names(res) <- names(X)
+    future::values(res)
 }
 
 fapply <- function(X, MARGIN, FUN, ...) {
-  fFUN <- function(...) { future(FUN(...)) }
-  res <- apply(X, MARGIN=1L, FUN=fFUN, ...)
-  res <- values(res)
-  sapply(res, FUN=I, simplify=TRUE)
+    if (!"future" %in% loadedNamespaces())
+        return(apply(X, MARGIN, FUN, ...))
+    fFUN <- function(...) { future::future(FUN(...)) }
+    res <- apply(X, MARGIN, FUN=fFUN, ...)
+    res <- future::values(res)
+    sapply(res, FUN=I, simplify=TRUE)
 }
+
+# EOF
