ac03e02dff3f066d09a90e37e17dbb3ebd6cdce6
Henrik Bengtsson <hb@aroma-project.org>
2021-10-02 09:58:31 -0700

    TESTS: Add package test for exportBins()

--- tests/QDNAseq.R
+++ tests/QDNAseq.R
@@ -1,10 +1,22 @@
-library("QDNAseq")
+library(QDNAseq)
+library(utils)
 
 # Load data
 data(LGG150)
 data <- LGG150
 print(data)
 stopifnot(inherits(data, "QDNAseqReadCounts"))
+
+# Exporting
+# FIXME:
+# formats = c("vcf", "seg") require 'calls'; give informative error message
+formats <- c("tsv", "igv", "bed")
+for (format in formats) {
+  file <- tempfile(fileext = sprintf(".%s", format))
+  exportBins(data, file = file, format = format)
+  stopifnot(file_test("-f", file))
+  file.remove(file)
+}
 
 # Plot isobars of read counts
 isobarPlot(data)
@@ -25,6 +37,17 @@ print(dataC)
 plot(dataC, ylim=c(-100, 200))
 stopifnot(inherits(dataC, "QDNAseqCopyNumbers"))
 
+# Exporting
+# FIXME:
+# formats = c("vcf", "seg") require 'calls'; give informative error message
+formats <- c("tsv", "igv", "bed")
+for (format in formats) {
+  file <- tempfile(fileext = sprintf(".%s", format))
+  exportBins(dataC, file = file, format = format)
+  stopifnot(file_test("-f", file))
+  file.remove(file)
+}
+
 # Normalize binned read counts to have diploid normal copy number
 dataN <- normalizeBins(dataC)
 print(dataN)
@@ -40,7 +63,30 @@ print(fit)
 plot(fit)
 stopifnot(inherits(fit, "QDNAseqCopyNumbers"))
 
+# Exporting
+# FIXME:
+# formats = c("vcf", "seg") require 'calls'; give informative error message
+formats <- c("tsv", "igv", "bed")
+for (format in formats) {
+  file <- tempfile(fileext = sprintf(".%s", format))
+  exportBins(fit, file = file, format = format)
+  stopifnot(file_test("-f", file))
+  file.remove(file)
+}
+
 # Call copy-number segments
-#fitC <- callBins(fit)
-#print(fitC)
-#plot(fitC)
+fitC <- callBins(fit)
+print(fitC)
+plot(fitC)
+
+# Exporting
+formats <- c("tsv", "igv", "bed", "vcf", "seg")
+for (format in formats) {
+  file <- tempfile(fileext = sprintf(".%s", format))
+  exportBins(fitC, file = file, format = format)
+  ## FIXME: formats = c("vcf", "seg") does not respect 'file'
+  if (!format %in% c("seg", "vcf")) {
+    stopifnot(file_test("-f", file))
+    file.remove(file)
+  }
+}
