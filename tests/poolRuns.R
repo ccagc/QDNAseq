cf88faf9553e6fb0c5b3755b1001ab8737c68825
Henrik Bengtsson <hb@aroma-project.org>
2023-01-13 13:54:03 -0800

    TESTS: Add poolRuns() that tests for bug #112

--- tests/poolRuns.R
+++ tests/poolRuns.R
@@ -1,9 +1,34 @@
 library(QDNAseq)
+library(Biobase)  ## sampleNames()
 
 data(LGG150)
-readCounts <- LGG150
 
-# Note: the following command will "pool" data from a single run, which
-# does not really test much
-pooledReadCounts <- poolRuns(readCounts, samples = "LGG150")
+message("poolRuns(LGG150) ...")
+pooledReadCounts <- poolRuns(LGG150, samples = sampleNames(LGG150))
 print(pooledReadCounts)
+message("poolRuns(LGG150) ... done")
+
+
+message("Create fake data set with two samples ...")
+phenodata <- phenoData(LGG150)
+phenodata@data <- rbind(phenodata@data, phenodata@data)
+
+counts <- assayDataElement(LGG150, "counts")
+counts2 <- cbind(counts, counts)
+colnames(counts2) <- sampleNames(phenodata)
+
+x <- new("QDNAseqReadCounts", bins = featureData(LGG150), phenodata = phenodata, counts = counts2)
+print(x)
+message("Create fake data set with two samples ... done")
+
+message("poolRuns(LGG150set) ...")
+x_pool <- poolRuns(x, samples = sampleNames(x))
+print(x_pool)
+
+## BUG #112 (https://github.com/ccagc/QDNAseq/issues/112)
+x_pool <- tryCatch({
+  poolRuns(x, samples = rep("pooled", times = 2L))
+}, error = identity)
+print(x_pool)
+
+message("poolRuns(LGG150set) ... done")
